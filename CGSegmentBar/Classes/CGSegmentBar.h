659a1470e1ffb9aa6f7950d6b62ef7cf4eb5fa8b
徐晨光 <xucg@Bsoft.com.cn>
2018-08-31 21:44:35 +0800

    注释修改为英文

--- CGSegmentBar/Classes/CGSegmentBar.h
+++ CGSegmentBar/Classes/CGSegmentBar.h
@@ -16,18 +16,18 @@ typedef void(^CGSegmentBarDidSelectItem)(NSUInteger index);
 
 @interface CGSegmentBar : UIView
 
-@property (nonatomic, strong) NSDictionary *textAttributes; // 标题设置
-@property (nonatomic, strong) NSDictionary *selectedTextAttributes; //!< 选中标题设置
+    @property (nonatomic, strong) NSDictionary *textAttributes; // nomal title attributes
+@property (nonatomic, strong) NSDictionary *selectedTextAttributes; // selected title attributes
 
-@property (nonatomic, strong) UIColor      *indicatorColor; // 选中指示条颜色.
-@property (nonatomic, assign) CGFloat      indicatorHeight; // 选中指示条高度
-@property (nonatomic, assign) BOOL         indicatorHidden; // 选中指示条隐藏
+@property (nonatomic, strong) UIColor      *indicatorColor;
+@property (nonatomic, assign) CGFloat      indicatorHeight;
+@property (nonatomic, assign) BOOL         indicatorHidden;
 
 @property (nonatomic, assign) UIEdgeInsets                paddingInsets;   // content padding
-@property (nonatomic, assign) CGFloat                     interitemSpacing;// 间距
+@property (nonatomic, assign) CGFloat                     interitemSpacing;// title interitem
 @property (nonatomic, assign) CGSegmentBarWidthStyle      widthStyle;
 
-@property (nonatomic, strong) NSArray<NSString *>    *titles; // 标题
+@property (nonatomic, strong) NSArray<NSString *>    *titles;
 @property (nonatomic, assign) NSInteger selectedIndex;
 @property (nonatomic, copy) CGSegmentBarDidSelectItem didSelectItemBlock;
 
